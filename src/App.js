d5986588ba673ff92db80345311b7aed8ad0c1c4
kishore Reddy <vvkishorereddy@gmail.com>
2018-08-23 00:27:30 +0530

    Inital setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component,Fragment } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment >
+      OK
+      </Fragment>
+    );
+  }
+}
+
+export default App;
